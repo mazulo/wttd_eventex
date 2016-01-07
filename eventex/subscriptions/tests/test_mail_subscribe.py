67397f10517fcd1dd6517de1099c4b27dfb7768a
Patrick Mazulo <pmazulo@gmail.com>
2016-01-07 12:08:20 -0300

    Refactor project

---
+++ eventex/subscriptions/tests/test_mail_subscribe.py
@@ -0,0 +1,34 @@
+from django.core import mail
+from django.test import TestCase
+
+
+class SubscribePostValid(TestCase):
+
+    def setUp(self):
+        data = dict(name='Patrick Mazulo', cpf='03286218383',
+                    email='pmazulo@gmail.com', phone='86-99988-7848')
+        self.resp = self.client.post('/inscricao/', data)
+        self.email = mail.outbox[0]
+
+    def test_subscription_email_subject(self):
+        expect = 'Confirmação de inscrição'
+        self.assertEqual(expect, self.email.subject)
+
+    def test_subscription_email_from(self):
+        expect = 'contato@eventex.com.br'
+        self.assertEqual(expect, self.email.from_email)
+
+    def test_subscription_email_to(self):
+        expect = ['contato@eventex.com.br', 'pmazulo@gmail.com']
+        self.assertEqual(expect, self.email.to)
+
+    def test_subscription_email_body(self):
+        contents = [
+            'Patrick Mazulo',
+            '03286218383',
+            'pmazulo@gmail.com',
+            '86-99988-7848',
+        ]
+        for content in contents:
+            with self.subTest():
+                self.assertIn(content, self.email.body)
--- readme.md
+++ readme.md
@@ -1,1 +1,40 @@
-Projeto do curso Welcome to the Django
+# Eventex
+
+Sistema de Eventos desenvolvido no curso Welcome to the Django
+
+## Como desenvolver?
+
+1. Clone o respositório
+2. Crieum virtualenv com Python 3.5
+3. Ative o virtualenv
+4. Instale as dependências
+5. Configure a instância com o .env
+6. Execute os testes
+
+````console
+git clone git@github.com:mazulo/eventex.git wttd
+cd wttd
+python -m venv .wttd
+source .wttd/bin/activate
+pip install -r requirements.txt
+cp contrib/env-sample .env
+python manage.py test
+````
+
+## Como fazer o deploy?
+
+1. Crie uma instância no heroku
+2. Envie as configurações para o heroku
+3. Defina uma SECRET_KEY segura para a instância
+4. Defina DEBUG=False
+5. Configure o serviço de email
+6. Envie o código para o heroku
+
+````console
+heroku create minhainstancia
+heroku configu:push
+heroku config:set SECRET_KEY=`python contrib/secret_gen.py`
+heroku config:set DEBUG=False
+# configura o email
+git push heroku master
+````
