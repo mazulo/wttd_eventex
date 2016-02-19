3995d8d2dc8d545240a81bad1ec3787eac409e0b
Patrick Mazulo <pmazulo@gmail.com>
2016-02-19 12:05:33 -0300

    Add keynote speakers

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'index.html')
 
+def home(request):
+    speakers = [
+        {'name': 'Grace Hopper', 'photo': 'http://hbn.link/hopper-pic'},
+        {'name': 'Alan Turing', 'photo': 'http://hbn.link/turing-pic'},
+    ]
+    return render(request, 'index.html', {'speakers': speakers})
